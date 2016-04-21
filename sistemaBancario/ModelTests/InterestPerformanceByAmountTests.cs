d19297e396521965d0d9d8612549c698a99b42f7
Mario Naranjo <mario15695@hotmail.com>
2016-04-21 14:06:28 -0600

    Version 1

---
+++ sistemaBancario/ModelTests/InterestPerformanceByAmountTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Tests
+{
+    [TestClass()]
+    public class InterestPerformanceByAmountTests
+    {
+        [TestMethod()]
+        public void InterestPerformanceByAmountTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void setStrategyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void calculateBalanceTest()
+        {
+            Assert.Fail();
+        }
+    }
+}

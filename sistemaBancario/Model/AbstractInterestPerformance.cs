df98602c7b3a69ca03506b2a904c8e38271c1155
Mario Naranjo <mario15695@hotmail.com>
2016-04-21 13:06:10 -0600

    Version 1

---
+++ sistemaBancario/Model/AbstractInterestPerformance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace Model
+{
+    public abstract class AbstractInterestPerformance
+    {
+        protected decimal _interestEarned;
+        protected double _annualInterest;
+
+        public decimal getInterestEarned
+        {
+            get
+            {
+                return _interestEarned;
+            }
+        }
+
+        public double getAnnualInterest
+        {
+            get
+            {
+                return _annualInterest;
+            }
+        }
+
+        public abstract decimal calculateBalance(int  pTermInDays);
+    }
+}
---
+++ sistemaBancario/sistemaBancario/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemaBancario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("hola");
+           Console.ReadKey();
+        }
+    }
+
+
+    abstract class abstractProductService {
+        abstract public void createVerify();
+        abstract public void createProduct();
+    }
+
+   class cuentaCorrienteFactory : abstractProductService {
+        public override void createVerify()
+        {
+            throw new NotImplementedException();
+        }
+        public override void createProduct()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class certificadoFactory : abstractProductService {
+        public override void createVerify()
+        {
+            throw new NotImplementedException();
+        }
+        public override void createProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    class pactoFactory : abstractProductService {
+        public override void createVerify()
+        {
+            throw new NotImplementedException();
+        }
+        public override void createProduct()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}

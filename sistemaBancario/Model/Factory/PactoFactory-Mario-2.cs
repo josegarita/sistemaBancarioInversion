ed99521ad598780acf11798f6a50995425b0bf62
Mario Naranjo <mario15695@hotmail.com>
2016-04-23 11:50:20 -0600

    Version 1

---
+++ sistemaBancario/Model/Factory/PactoFactory-Mario-2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Verifiers;
+using Model.Products;
+
+
+namespace Model.Factory
+{
+    class PactoFactory : AbstractProductService
+    {
+        public override ServiceVerifier createVerify()
+        {
+            InversionVistaPactadaVerifier cuenta = new InversionVistaPactadaVerifier();
+            return cuenta;
+           
+        }
+
+        public override SavingInvestementProduct createProduct()
+        {
+            InversionPactada cuenta = new InversionPactada();
+            return cuenta;
+        }
+    }
+}

aab9707b1e95994527de453ba88a03eedaf4b1f5
Mario Naranjo <mario15695@hotmail.com>
2016-04-23 11:38:48 -0600

    Version 1

---
+++ sistemaBancario/Model/Factory/certificadoFactory-Mario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Verifiers;
+using Model.Products;
+
+namespace Model.Factory
+{
+    class certificadoFactory : abstractProductService
+    {
+        public override ServiceVerifier createVerify()
+        {
+            CertificadoDeDepositoVerifier cuenta = new CertificadoDeDepositoVerifier();
+            return cuenta;
+           
+        }
+        public override SavingInvestementProduct createProduct()
+        {
+            CentificadoDeposito cuenta = new CentificadoDeposito();
+            return cuenta;
+           
+        }
+
+    }
+
+}
